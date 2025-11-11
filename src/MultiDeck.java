dbb1fdd7cd37f87969d6985df7e2943a1a8265f5
vm.alonsobarberan <vm.alonsobarberan@edu.gva.es>
2025-11-11 18:53:10 +0100

    MultiDeck constructor

--- src/MultiDeck.java
+++ src/MultiDeck.java
@@ -3,8 +3,14 @@ public class MultiDeck {
 
     public MultiDeck(int numDecks) {
         cards = new Card[52 * numDecks];
+        int counter = 0;
         for (int numDeck = 1; numDeck <= numDecks; numDeck++) {
-            
+            for(int numSuit = 0; numSuit < 4; numSuit++) {
+                for (int number = 1; number <= 13; number++) {
+                    cards[counter] = new Card(number, Card.SUITS[numSuit]);
+                    counter++;
+                }
+            }
         }
     }
 }
