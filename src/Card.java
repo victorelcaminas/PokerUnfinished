c27dc6691f37d507a3efc3f6d2899cbbc3ff0bc1
vm.alonsobarberan <vm.alonsobarberan@edu.gva.es>
2025-11-11 18:26:31 +0100

    Initial commit

---
+++ src/Card.java
@@ -0,0 +1,29 @@
+public class Card {
+    private int number;
+    private String suit;
+
+    public static final String[] SUITS = {"♠", "♥", "♦", "♣"};
+
+    public Card(int number, String suit) {
+        this.number = number;
+        this.suit = suit;
+    }
+
+    @Override
+    public String toString() {
+        String s = "";
+        if (number == 1) {
+            s += "A";
+        } else if (number <= 10) {
+            s += number;
+        } else if (number == 11) {
+            s += "J";
+        } else if (number == 12) {
+            s += "Q";
+        } else {
+            s += "K";
+        }
+        s += " de " + suit;
+        return s;
+    }
+}
---
+++ src/MultiDeck.java
@@ -0,0 +1,10 @@
+public class MultiDeck {
+    private Card[] cards;
+
+    public MultiDeck(int numDecks) {
+        cards = new Card[52 * numDecks];
+        for (int numDeck = 1; numDeck <= numDecks; numDeck++) {
+            
+        }
+    }
+}
