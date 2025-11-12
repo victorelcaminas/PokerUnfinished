e55cdaf81f392248388e93d46f8ce7b2beb677ff
vm.alonsobarberan <vm.alonsobarberan@edu.gva.es>
2025-11-12 19:16:37 +0100

    Finished finished

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,32 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String answer;
+        boolean gameOver = false;
         MultiDeck mazo = new MultiDeck(2);
         mazo.shuffle();
         Player player = new Player("Víctor");
         player.dealCard(mazo.extractCard());
         player.dealCard(mazo.extractCard());
-        System.out.println(player);
+
+        while (!gameOver) {
+            System.out.println(player);
+            System.out.println("Score: " + player.getScore());
+            System.out.println("Another card (Y/N)?");
+            answer = input.next().toUpperCase();
+            if (answer.equals("Y")) {
+                player.dealCard(mazo.extractCard());
+                if (player.getScore() > 21) {
+                    System.out.println(player);
+                    System.out.println("Score :" + player.getScore());
+                    System.out.println("You loose !!!");
+                    gameOver = true;
+                }
+            } else if (answer.equals("N")) {
+                gameOver = true;
+            }
+        }
     }
 }
